d440b4600ea9aa17af1407bcbf5f6efe92f65a35
재현 박 <1234qkr@gmail.com>
2022-03-13 15:15:01 +0900

    add size val

--- source/ANN.py
+++ source/ANN.py
@@ -9,14 +9,16 @@ class Neuron(object):
     Attributes:
         W (ndarray): 각 입력에 대한 가중치
         b (float): 편항값, 가중합에 더해짐
+        size (int): 계층의 크기 / 뉴런의 개수
         activation_fn (callable): 활성화 함수
     """
 
-    def __init__(self, num_inputs, activation_fn):
+    def __init__(self, num_inputs, layer_size ,activation_fn):
         super().__init__()
         #랜덤값으로 가중치 벡터와 편향값을 초기화함
-        self.W = np.random(num_inputs)
-        self.b = np.random.rand(1)
+        self.W = np.random.standard_normal((num_inputs),layer_size)
+        self.b = np.random.standard_normal(layer_size)
+        self.size = layer_size
         self.activation_fn = activation_fn
 
     def forward(self,x):
