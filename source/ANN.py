0a4973aef7733a3d04c869dc1039a31d05ba1c9a
재현 박 <1234qkr@gmail.com>
2022-03-13 10:36:29 +0900

    add ann.py & move file

---
+++ source/ANN.py
@@ -0,0 +1,25 @@
+#-*- coding:cp949-*-
+import numpy as np
+
+class Neuron(object):
+    """간단한 전방 전달 인공 뉴런
+    Args:
+        num_input (int): 입력 벡터 크기 / 입력 값 개수.
+        activation_fn (callable): 활성화 함수
+    Attributes:
+        W (ndarray): 각 입력에 대한 가중치
+        b (float): 편항값, 가중합에 더해짐
+        activation_fn (callable): 활성화 함수
+    """
+
+    def __init__(self, num_inputs, activation_fn):
+        super().__init__()
+        #랜덤값으로 가중치 벡터와 편향값을 초기화함
+        self.W = np.random(num_inputs)
+        self.b = np.random.rand(1)
+        self.activation_fn = activation_fn
+
+    def forward(self,x):
+        #뉴런을 통해 입력 신호를 전달
+        z = np.dot(x,self.W) + self.b
+        return self.activation_fn(z)
